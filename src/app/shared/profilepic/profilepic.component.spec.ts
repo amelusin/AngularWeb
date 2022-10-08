c67420df53420fbf29f98ee269f81b2d3070edcc
amelusin <rodriadri@hotmail.com>
2022-10-08 20:24:56 -0300

    agregamos modulo banner con transicion fade, modulo foto de perfil flotante, y modulo container aboutme

---
+++ src/app/shared/profilepic/profilepic.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfilepicComponent } from './profilepic.component';
+
+describe('ProfilepicComponent', () => {
+  let component: ProfilepicComponent;
+  let fixture: ComponentFixture<ProfilepicComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProfilepicComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProfilepicComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
-
+import { BannerComponent } from './banner/banner.component';
+import { ProfilepicComponent } from './profilepic/profilepic.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    ProfilepicComponent,
+    AboutmeComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[HeaderComponent]
+  exports:[
+    HeaderComponent,
+    BannerComponent,
+    ProfilepicComponent,
+    AboutmeComponent
+  ]
 })
 export class SharedModule { }
